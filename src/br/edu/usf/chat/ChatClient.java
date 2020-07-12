550dffb113afcdf975c8b95d2539ecb5394832fc
Eduwardo HJoribe <eduhoribe@gmail.com>
2020-07-12 17:14:20 -0300

    a

--- src/br/edu/usf/chat/ChatClient.java
+++ src/br/edu/usf/chat/ChatClient.java
@@ -1,8 +1,10 @@
 package br.edu.usf.chat;
 
-import java.net.*;
-import java.io.*;
- 
+import javax.net.ssl.SSLSocketFactory;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * This is the chat client program.
  * Type 'bye' to terminte the program.
@@ -13,45 +15,62 @@ public class ChatClient {
     private String hostname;
     private int port;
     private String userName;
- 
+
     public ChatClient(String hostname, int port) {
         this.hostname = hostname;
         this.port = port;
     }
- 
-    public void execute() {
-        try {
-            Socket socket = new Socket(hostname, port);
- 
-            System.out.println("Connected to the chat server");
- 
-            new ReadThread(socket, this).start();
-            new WriteThread(socket, this).start();
- 
-        } catch (UnknownHostException ex) {
-            System.out.println("Server not found: " + ex.getMessage());
-        } catch (IOException ex) {
-            System.out.println("I/O Error: " + ex.getMessage());
+
+    public void execute(boolean ssl) {
+
+        if (ssl) {
+
+            try (final Socket socket = SSLSocketFactory.getDefault().createSocket(hostname, port)) {
+
+                System.out.println("Connected to the chat server");
+
+                new ReadThread(socket, this).start();
+                new WriteThread(socket, this).start();
+
+            } catch (UnknownHostException ex) {
+                System.out.println("Server not found: " + ex.getMessage());
+            } catch (IOException e) {
+                System.out.println("I/O Error: " + e.getMessage());
+            }
+
+        } else {
+            try {
+                Socket socket = new Socket(hostname, port);
+
+                System.out.println("Connected to the chat server");
+
+                new ReadThread(socket, this).start();
+                new WriteThread(socket, this).start();
+
+            } catch (UnknownHostException ex) {
+                System.out.println("Server not found: " + ex.getMessage());
+            } catch (IOException ex) {
+                System.out.println("I/O Error: " + ex.getMessage());
+            }
         }
- 
     }
- 
+
     void setUserName(String userName) {
         this.userName = userName;
     }
- 
+
     String getUserName() {
         return this.userName;
     }
- 
- 
+
     public static void main(String[] args) {
         if (args.length < 2) return;
- 
+        System.setProperty("javax.net.ssl.trustStore", "chat.store");
+
         String hostname = args[0];
         int port = Integer.parseInt(args[1]);
- 
+
         ChatClient client = new ChatClient(hostname, port);
-        client.execute();
+        client.execute(true);
     }
 }
--- src/br/edu/usf/chat/ChatServer.java
+++ src/br/edu/usf/chat/ChatServer.java
@@ -1,9 +1,12 @@
 package br.edu.usf.chat;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
- 
+import javax.net.ssl.SSLServerSocketFactory;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * This is the chat server program.
  * Press Ctrl + C to terminate the program.
@@ -14,44 +17,66 @@ public class ChatServer {
     private int port;
     private Set<String> userNames = new HashSet<>();
     private Set<UserThread> userThreads = new HashSet<>();
- 
+
     public ChatServer(int port) {
         this.port = port;
     }
- 
-    public void execute() {
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
- 
-            System.out.println("Chat Server is listening on port " + port);
- 
-            while (true) {
-                Socket socket = serverSocket.accept();
-                System.out.println("New user connected");
- 
-                UserThread newUser = new UserThread(socket, this);
-                userThreads.add(newUser);
-                newUser.start();
- 
+
+    public void execute(boolean ssl) {
+        if (ssl) {
+
+            try (final ServerSocket serverSocket = SSLServerSocketFactory.getDefault().createServerSocket(8989)) {
+                System.out.println("Chat Server is listening on port " + port);
+
+                while (true) {
+                    final Socket accept = serverSocket.accept();
+                    System.out.println("New user connected");
+
+                    UserThread newUser = new UserThread(accept, this);
+                    userThreads.add(newUser);
+                    newUser.start();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
- 
-        } catch (IOException ex) {
-            System.out.println("Error in the server: " + ex.getMessage());
-            ex.printStackTrace();
+
+        } else {
+            try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+                System.out.println("Chat Server is listening on port " + port);
+
+                while (true) {
+                    Socket socket = serverSocket.accept();
+                    System.out.println("New user connected");
+
+                    UserThread newUser = new UserThread(socket, this);
+                    userThreads.add(newUser);
+                    newUser.start();
+
+                }
+
+            } catch (IOException ex) {
+                System.out.println("Error in the server: " + ex.getMessage());
+                ex.printStackTrace();
+            }
         }
     }
- 
+
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("Syntax: java ChatServer <port-number>");
             System.exit(0);
         }
- 
+
+        System.setProperty("javax.net.ssl.keyStore", "chat.store");
+        System.setProperty("javax.net.ssl.keyStorePassword", "abcd1234");
+
         int port = Integer.parseInt(args[0]);
- 
+
         ChatServer server = new ChatServer(port);
-        server.execute();
+        server.execute(true);
     }
- 
+
     /**
      * Delivers a message from one user to others (broadcasting)
      */
@@ -62,14 +87,14 @@ public class ChatServer {
             }
         }
     }
- 
+
     /**
      * Stores username of the newly connected client.
      */
     void addUserName(String userName) {
         userNames.add(userName);
     }
- 
+
     /**
      * When a client is disconneted, removes the associated username and UserThread
      */
@@ -80,11 +105,11 @@ public class ChatServer {
             System.out.println("The user " + userName + " quitted");
         }
     }
- 
+
     Set<String> getUserNames() {
         return this.userNames;
     }
- 
+
     /**
      * Returns true if there are other users connected (not count the currently connected user)
      */
